6271620fcc5fe92fcd980580cd0a727d37ad35fe
Maxime <Maxime@MaximePc.local>
2017-08-23 13:36:44 -0400

    ok

--- Lab1/pages/headers/admin.php
+++ Lab1/pages/headers/admin.php
@@ -21,6 +21,7 @@ require_once '../../system/header.php';
       </a>
  	 <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
+        
          
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
